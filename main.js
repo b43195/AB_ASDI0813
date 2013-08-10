27102148b2c4a9349a6e30e113ccb61e6e7b20bd
Andisheh Bassiri <andishake@fullsail.edu>
2013-08-09 21:27:57 -0500

    Visual Code Complete - minus few items that need to be researched

--- main.js
+++ main.js
@@ -2,3 +2,38 @@
 // 0813
 // ASDI
 
+$(#activeProj).click(function(){
+	/*When active projects is selected, loop 
+	through and select all project with a "active" status
+	and populate list with active projects*/
+});
+
+$(#completedProj).click(function(){
+	/*When completed projects is selected, loop 
+	through and select all project with a "complete" status
+	and populate list with completed projects*/
+});
+
+var newProj = function(){
+	//create project from form data with default active state
+};
+
+var newTask = function(){
+	//create task from form data and add to associated project
+};
+
+var editProj = function(){
+	//pull project from memory and edit all details execpt tasks
+};
+
+var editTask = function(){
+	//pull task from memory and edit all details
+};
+
+var clearEntries = function(){
+	//ARE YOUR SURE?? Then clears all entries from memory
+};
+
+var popProject = function(){
+	//Populates project data from memory onto specProjPage and adds edit/delete buttons
+};
